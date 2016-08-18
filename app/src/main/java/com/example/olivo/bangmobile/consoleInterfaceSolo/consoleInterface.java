77c0adb44f2ffaa78c12073165a0a1802bf7e7c2
sytha <sytha38@gmail.com>
2016-08-18 14:33:18 +0200

    Ajout de la classe d'interface en mode console

---
+++ app/src/main/java/com/example/olivo/bangmobile/consoleInterfaceSolo/consoleInterface.java
@@ -0,0 +1,62 @@
+package com.example.olivo.bangmobile.consoleInterfaceSolo;
+
+import android.content.Context;
+
+import com.example.olivo.bangmobile.gameMechanics.Game;
+import com.example.olivo.bangmobile.gameMechanics.elements.Figure;
+import com.example.olivo.bangmobile.gameMechanics.elements.Player;
+import com.example.olivo.bangmobile.gameMechanics.elements.Role;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Created by olivo on 10/08/2016.
+ */
+public class ConsoleInterface {
+    Game game;
+Context context;
+    public ConsoleInterface(Context context){
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(new Player(0, "p1"));
+        players.add(new Player(1, "p2"));
+        players.add(new Player(2, "p3"));
+        players.add(new Player(3, "p4"));
+        this.context=context;
+        game = new Game(context, players );
+        game.startGame();
+    }
+
+
+
+    public String getSecretPlayersInfo(){
+        String info = "";
+        for(Map.Entry e : game.players.entrySet()){
+            Player p =  (Player) e.getValue();
+            info += "\npos :" + e.getKey() + " info : " + p.id + ", " + p.figure.id + ", "+ p.role + ", " + p.healthPoint + "/" + p.maxHealthPoint ;
+        }
+
+        return info;
+    }
+
+    public String getPlayersInfo(){
+        String info = "";
+        for(Map.Entry e : game.players.entrySet()){
+            Player p =  (Player) e.getValue();
+            info += "\n" + p.name + "("+p.figure.id +")";
+            if(p.role == Role.SHERIF){
+                info += " (SHERIF)";
+            }
+            info += "\nHP :" + p.healthPoint + "/" + p.maxHealthPoint;
+            info += " Vision :" + p.vision;
+            info += " WVision :" + p.weaponVision;
+            info += " Evasion :" + p.evasion;
+
+            info += "\nHand : " + p.handCards.size();
+            info += "\n";
+        }
+
+        return info;
+    }
+
+}
